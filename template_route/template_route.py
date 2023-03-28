b31b4cb998504ca6e2199e6f1b38ade548d14f25
sxkxixx <sasha.kornilov.1212@gmail.com>
2023-03-28 15:56:40 +0500

    Добавил ручку для отображения шаблонов. Изменил авторизацию на cookie

--- template_route/template_route.py
+++ template_route/template_route.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
+from fastapi.exceptions import HTTPException
+from user.api import get_current_user
+from user.models import User
 
 template_route = APIRouter()
 templates = Jinja2Templates(directory='templates')
@@ -13,3 +16,10 @@ def login_page(request: Request):
 @template_route.get('/register')
 def register(request: Request):
     return templates.TemplateResponse('register.html', context={'request': request})
+
+
+@template_route.get('/profile')
+async def profile(request: Request, user: User = Depends(get_current_user)):
+    if not user:
+        raise HTTPException(status_code=401, detail='Пожалуйста, авторизуйтесь или зарегистрируйтесь.')
+    return templates.TemplateResponse('profile.html', context={'request': request, 'user': user})
--- user/api.py
+++ user/api.py
@@ -1,13 +1,8 @@
-from datetime import timedelta
-from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException, Response, Request
-from fastapi.security import OAuth2PasswordRequestForm
-from fastapi import status
-from user.auth import get_current_active_user, authenticate_user, create_access_token
+from fastapi import APIRouter, Depends, Response, Request, HTTPException
+from fastapi.responses import HTMLResponse
 from user.hasher import Hasher
 from user.models import User
-from user.schemas import Token
-from user.auth_data import ACCESS_TOKEN_EXPIRE_MINUTES, ALGORITHM, SECRET_KEY
+from user.auth_data import ALGORITHM, SECRET_KEY, ACCESS_TOKEN_EXPIRE_MINUTES
 from fastapi.templating import Jinja2Templates
 from jose import jwt
 
@@ -15,20 +10,13 @@ user_route = APIRouter()
 templates = Jinja2Templates(directory='templates')
 
 
-@user_route.post("/token/login", response_model=Token)
-async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    user = authenticate_user(form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
+async def get_current_user(request: Request):
+    try:
+        username = jwt.decode(request.cookies.get('access_token'), SECRET_KEY, algorithms=[ALGORITHM])['sub']
+        user = User.select().where(User.username == username).get()
+        return user
+    except:
+        return None
 
 
 @user_route.post('/register')
@@ -53,7 +41,8 @@ async def register(request: Request):
             role=1,
         )
         user.save()
-        return templates.TemplateResponse('login.html', {'request': request, 'message': f'Аккаунт {user.username} успешно создан.'})
+        return templates.TemplateResponse('login.html',
+                                          {'request': request, 'message': f'Аккаунт {user.email} успешно создан.'})
     except:
         return templates.TemplateResponse('register.html', {'request': request, 'message': 'Что-то пошло не так'})
 
@@ -68,7 +57,6 @@ async def login(response: Response, request: Request):
         return templates.TemplateResponse('login.html', {'request': request, "errors": errors})
     try:
         user = User.select().where(username == User.username).get()
-        print(user)
         if not user:
             errors.append('Нет пользователя с таким именем.')
             return templates.TemplateResponse('login.html', {'request': request, 'errors': errors})
@@ -77,13 +65,16 @@ async def login(response: Response, request: Request):
             token = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
             message = 'Вход успешно выполнен'
             response = templates.TemplateResponse('login.html', {'request': request, 'message': message})
-            response.set_cookie(key='access_token', value=f'Bearer {token}', httponly=True)
+            response.set_cookie(key='access_token', value=token, httponly=True, expires=ACCESS_TOKEN_EXPIRE_MINUTES)
             return response
     except:
         errors.append('Ой... Что-то пошло не так.')
         return templates.TemplateResponse('login.html', {'request': request, 'errors': errors})
 
 
-@user_route.get('/users/me')
-async def read_users_me(current_user: Annotated[str, Depends(get_current_active_user)]):
-    return current_user
+@user_route.get('/user/me')
+async def current_user(user: User = Depends(get_current_user)):
+    if not user:
+        raise HTTPException(status_code=401, detail='Пожалуйста, авторизуйтесь или зарегистрируйтесь.')
+
+    return HTMLResponse(f'user: {user.username}')
